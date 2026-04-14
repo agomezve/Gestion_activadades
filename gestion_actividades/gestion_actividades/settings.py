2d2bdaf2cc6debc41f80737848c3045a1676ea3b
Victor <vgarciacr.inf@upsa.es>
2026-04-14 12:49:54 +0200

    settings

--- gestion_actividades/gestion_actividades/settings.py
+++ gestion_actividades/gestion_actividades/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_actividades.apps.AppActividadesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
